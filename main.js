98f111fd350102e3dc34afad2c696f12d9b1d1b0
Aaden06 <80853681+Aaden06@users.noreply.github.com>
2021-07-13 19:35:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function domyfunction(){
+    document.getElementById("firstname").innerHTML = "";
+    document.getElementById("lastname").innerHTML = "";
+    document.getElementById("pn").innerHTML = "";
+    document.getElementById("email").innerHTML = "";
+    document.getElementById("card").innerHTML = "";
+    document.getElementById("state").innerHTML = "";
+    document.getElementById("city").innerHTML = "";
+    document.getElementById("road").innerHTML = "";
+    document.getElementById("streetnumber").innerHTML = "";
+    document.getElementById("last").innerHTML = "Congratulations on getting a free PS5";
+}
